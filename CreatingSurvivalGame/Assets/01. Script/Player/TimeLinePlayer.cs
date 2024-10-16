80ae042b8d153ce239bc2c9e049eec506a4f907b
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-10-16 12:22:27 +0900

    타임라인 컷씬 제작
    - 타임라인을 사용해서 컷씬을 제작해봄.

---
+++ CreatingSurvivalGame/Assets/01. Script/Player/TimeLinePlayer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeLinePlayer : MonoBehaviour
+{
+
+
+    private void OnFootstep(AnimationEvent animationEvent)
+    {
+
+    }
+}
